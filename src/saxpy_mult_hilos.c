a25c645142b34007e59c3b93e3648413ffd7ca95
YulyAR1 <yuly.alvear@udea.edu.co>
2024-05-10 09:35:58 -0500

    Saxpy_iteration Calcular_tiempo Imprimir_Resultados Mostrar_Presentacion Limpiar_Pantalla

--- src/saxpy_mult_hilos.c
+++ src/saxpy_mult_hilos.c
@@ -72,3 +72,63 @@ void inicializar_datos(double** X, double** Y, double** Y_avgs, int p, int num_i
         (*Y_avgs)[i] = 0.0;
     }
 }
+
+void* saxpy_iteration(void* arg) {
+    struct datos_hilo* datos = (struct datos_hilo*)arg;
+    int tamano = datos->p / datos->n_threads;
+    int indice_inicio = datos->id_hilo * tamano;
+    int indice_final = (datos->id_hilo == datos->n_threads - 1) ? datos->p - 1 : indice_inicio + tamano - 1; 
+    for (int it = 0; it < datos->max_iters; it++) {
+        double sum = 0.0;
+        for (int i = indice_inicio; i <= indice_final; i++) {
+            datos->Y[i] = datos->Y[i] + datos->a * datos->X[i];
+            sum += datos->Y[i];  
+        }
+        // Suma atómica
+        datos->Y_avgs[it] = datos->Y_avgs[it] + sum;
+       // atomic_exchange(&datos->Y_avgs[it], atomic_load(&datos->Y_avgs[it]) + sum);
+        // Divide la suma por el tamaño del bloque
+        datos->Y_avgs[it] = datos->Y_avgs[it] / (double)(indice_final - indice_inicio + 1);
+       // atomic_exchange(&datos->Y_avgs[it], atomic_load(&datos->Y_avgs[it]) / (double)(indice_final - indice_inicio + 1));
+    }
+
+    pthread_exit(NULL);
+}
+
+double calcular_tiempo(struct timeval t_inicio, struct timeval t_final) {
+    double tiempo = (t_final.tv_sec - t_inicio.tv_sec) * 1000.0;  
+    tiempo += (t_final.tv_usec - t_inicio.tv_usec) / 1000.0;
+    return tiempo;
+}
+
+void imprimir_resultados(double* Y, double* Y_avgs, int p, int max_iters) {
+    printf("Últimos 3 valores de Y: %f, %f, %f \n", Y[p-3], Y[p-2], Y[p-1]);
+    printf("Últimos 3 valores de Y_avgs: %f, %f, %f \n", Y_avgs[max_iters-3], Y_avgs[max_iters-2], Y_avgs[max_iters-1]);
+}
+
+void mostrarPresentacion() {
+    limpiarPantalla(); 
+    printf("\n\n");
+    printf("*****************************************************************\n");
+    printf("*             Bienvenido al Programa Saxpy                      *\n");
+    printf("*                                                               *\n");
+    printf("*             TRABAJO HECHO POR Yuly Alvear Romo                *\n");
+    printf("*             TRABAJO HECHO POR Leidy Castaño                   *\n");
+    printf("*             TRABAJO HECHO POR Omar alberto Torres             *\n");
+    printf("*                                                               *\n");
+    printf("*****************************************************************\n");
+    printf("\n");
+    printf("Iniciando programa...\n");
+    printf("\n");
+    printf("Cargando ");
+    for (int i = 0; i < 10; i++) {
+        printf(".");
+        fflush(stdout); 
+        usleep(300000); 
+    }
+    printf("\n\n");
+}
+
+void limpiarPantalla() {
+    printf("\033[2J\033[H");  
+}
